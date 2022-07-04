4f28adeea90638af27dfdde976ba2930446356bb
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-07-03 22:09:53 -0500

    add preventDefault

--- js/index.js
+++ js/index.js
@@ -2,8 +2,20 @@
 // you can also load the code snippets in using your browser's console
 
 let main = document.getElementById('main')
-main.addEventListener('click', clickAlert)
-
-function clickAlert(event) {
+main.addEventListener('click', function (event) {
     alert('I was clicked!')
-}
+})
+
+
+
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+    if (e.which === 71) {
+        console.log('default prevented');
+        return e.preventDefault();
+      } else {
+        console.log('Not a "g"');
+      }
+
+});
